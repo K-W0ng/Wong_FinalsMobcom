d3b86a11d72c2426044fb304faa6e684629a52fb
User <20085952@s.ubaguio.edu>
2022-12-01 03:46:54 +0800

    ui for main, login, and register

--- app/src/main/java/edu/mobcom/wong_finalsmobcom/LoginActivity.kt
+++ app/src/main/java/edu/mobcom/wong_finalsmobcom/LoginActivity.kt
@@ -1,11 +1,33 @@
 package edu.mobcom.wong_finalsmobcom
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private  lateinit var btnSignUpLogin: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnSignUpLogin = findViewById(R.id.btnSignUpLogin)
+
+        fun openRegisterActivity(){
+            val iRegistrationActivity = Intent(this, RegistrationActivity::class.java)
+            startActivity(iRegistrationActivity)
+        }
+
+        btnSignUpLogin.setOnClickListener {
+            openRegisterActivity()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/edu/mobcom/wong_finalsmobcom/MainActivity.kt
+++ app/src/main/java/edu/mobcom/wong_finalsmobcom/MainActivity.kt
@@ -1,11 +1,33 @@
 package edu.mobcom.wong_finalsmobcom
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var proceedMain: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        proceedMain = findViewById(R.id.btnProceed)
+
+        fun openLoginActivity(){
+            val iLoginActivity = Intent(this, LoginActivity::class.java)
+            startActivity(iLoginActivity)
+        }
+
+        proceedMain.setOnClickListener {
+            openLoginActivity()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
