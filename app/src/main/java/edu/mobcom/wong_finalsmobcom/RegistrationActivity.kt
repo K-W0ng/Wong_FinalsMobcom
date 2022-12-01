a22faf9fffb7042a8053071a2e06705edb8ea776
User <20085952@s.ubaguio.edu>
2022-12-02 00:17:26 +0800

    inflate not working

--- app/src/main/java/edu/mobcom/wong_finalsmobcom/RegistrationActivity.kt
+++ app/src/main/java/edu/mobcom/wong_finalsmobcom/RegistrationActivity.kt
@@ -1,11 +1,24 @@
 package edu.mobcom.wong_finalsmobcom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
+import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding : RegistrationActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding = RegistrationActivity.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
--- app/src/main/java/edu/mobcom/wong_finalsmobcom/UserProfileActivity.kt
+++ app/src/main/java/edu/mobcom/wong_finalsmobcom/UserProfileActivity.kt
@@ -2,10 +2,14 @@ package edu.mobcom.wong_finalsmobcom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
 
 class UserProfileActivity : AppCompatActivity() {
+    private lateinit var binding : UserProfileActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = RegistrationActivity.inflate
         setContentView(R.layout.activity_user_profile)
     }
 }
