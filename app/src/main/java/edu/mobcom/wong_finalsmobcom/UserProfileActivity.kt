9c83888cc7ad5aaf6fa54a28b0284aa8d388bf7e
User <20085952@s.ubaguio.edu>
2022-12-01 18:55:50 +0800

    ui for main, login, and register

---
+++ app/src/main/java/edu/mobcom/wong_finalsmobcom/UserProfileActivity.kt
@@ -0,0 +1,11 @@
+package edu.mobcom.wong_finalsmobcom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
